2419680b9575c988f0f6770374fd7868e271f694
maanin-kala <lauri.maaninka@gmail.com>
2025-03-10 21:51:27 +0200

    Update Projektikertomus

--- src/main/java/com/example/harjoitustyo/Tehtavat.java
+++ src/main/java/com/example/harjoitustyo/Tehtavat.java
@@ -6,13 +6,17 @@ import java.util.ArrayList;
 public class Tehtavat {
     private static final String tiedostoPolku = "tehtavat.dat";
 
+    /**
+     * Hakee ja lataa tehtävät luokassa määritellystä tiedostopolusta
+     * @return ArrayList&lt;Tehtava>
+     */
     public static ArrayList<Tehtava> getTehtavat() {
         ArrayList<Tehtava> lista = new ArrayList<>();
 
         try (FileInputStream fis = new FileInputStream(tiedostoPolku);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
             Object luettu = ois.readObject();
-            if  (luettu instanceof ArrayList<?>) {
+            if (luettu instanceof ArrayList<?>) {
                 lista = (ArrayList<Tehtava>) luettu;
             }
         } catch (FileNotFoundException e) {
@@ -23,6 +27,10 @@ public class Tehtavat {
         return lista;
     }
 
+    /**
+     * Tallentaa tehtävälistan luokkaan määriteltyyn tiedostoon
+     * @param lista ArrayList&lt;Tehtava>
+     */
     public static void tallennaTehtavat(ArrayList<Tehtava> lista) {
         try (FileOutputStream fos = new FileOutputStream(tiedostoPolku);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
